a848a074e3060bc784fd16baf272264fdd41fe86
nikita evdokimov <evdokimovnikita1@gmail.com>
2022-10-08 15:04:53 +0300

    network manager

--- GHFollowers/Screens/FollowersListVC.swift
+++ GHFollowers/Screens/FollowersListVC.swift
@@ -14,6 +14,19 @@ class FollowersListVC: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
+        
+        NetworkManager.shared.getFolowers(for: username, page: 1) { result in
+            
+            switch result {
+                
+            case .success(let followers):
+                print(followers.count)
+                print(followers)
+                
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "TEst", message: error.rawValue, buttonTitle: "OK")
+            }
+        }
     }
 
     override func viewDidLoad() {
---
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessage.swift
+//  GHFollowers
+//
+//  Created by Nikita Evdokimov on 07.10.22.
+//
+
+import Foundation
+
+    enum GFError: String, Error {
+    case invalidUsername    = "Этот пользователь создает неправльный запрос. Попробуй снова."
+    case unableToComplete   = "Невозможно закончить твой запрос, пожалуйста проверь интернет соединение."
+    case invalidResponse    = "Неправильный ответ от сервера, попробуй еще раз."
+    case invalidData        = "Данные с сервера некоректны, попробуй снова."
+}
