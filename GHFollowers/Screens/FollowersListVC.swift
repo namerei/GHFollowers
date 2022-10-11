170840fc1a1e91d395cc10b3a8d0323cbcf8d6f2
nikita evdokimov <evdokimovnikita1@gmail.com>
2022-10-11 18:48:18 +0300

    userinfoVC start

--- GHFollowers/Screens/FollowersListVC.swift
+++ GHFollowers/Screens/FollowersListVC.swift
@@ -16,6 +16,7 @@ class FollowersListVC: UIViewController {
     var filteredFollowers   = [Follower]()
     var page                = 1
     var hasMoreFollowers    = true
+    var isSearching         = false
     
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
@@ -114,6 +115,16 @@ extension FollowersListVC: UICollectionViewDelegate {
             getFollowers(username: username, page: page)
         }
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let activeArray     = isSearching ? filteredFollowers : followers
+        let follower        = activeArray[indexPath.item]
+        
+        let destVC          = UserInfoVC()
+        destVC.username     = follower.login
+        let navController   = UINavigationController(rootViewController: destVC)
+        present(navController, animated: true)
+    }
 }
 
 //MARK: - UISearchResultUpdating, UISearchBarDelegate
@@ -121,11 +132,13 @@ extension FollowersListVC: UISearchResultsUpdating, UISearchBarDelegate {
     
     func updateSearchResults(for searchController: UISearchController) {
         guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
-        filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
+        isSearching         = true
+        filteredFollowers   = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
         updateData(on: filteredFollowers)
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = false
         updateData(on: followers)
     }
 }
---
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,26 @@
+//
+//  UserInfoVC.swift
+//  GHFollowers
+//
+//  Created by Nikita Evdokimov on 11.10.22.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+        
+        print(username)
+    }
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+}
