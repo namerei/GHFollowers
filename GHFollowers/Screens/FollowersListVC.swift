8a218af5718a44706b96cc0866017418e43e10de
nikita evdokimov <evdokimovnikita1@gmail.com>
2022-10-13 22:10:12 +0300

    optimization followerListVC: smuther animation when go to follower from user

--- GHFollowers/Screens/FollowersListVC.swift
+++ GHFollowers/Screens/FollowersListVC.swift
@@ -202,7 +202,7 @@ extension FollowersListVC: FollowerListVCDelegate {
         page            = 1
         followers.removeAll()
         filteredFollowers.removeAll()
-        collectionView.setContentOffset(.zero, animated: true)
+        collectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .top, animated: true)
         getFollowers(username: username, page: page)
     }
 }
