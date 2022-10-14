76c2b4f8e7e01af99cb838f95847e7abf930b3b6
nikita evdokimov <evdokimovnikita1@gmail.com>
2022-10-14 19:07:49 +0300

    optimization: uitableView extension

---
+++ GHFollowers/Extensions/UITableView+Ext.swift
@@ -0,0 +1,15 @@
+//
+//  UITableView+Ext.swift
+//  GHFollowers
+//
+//  Created by Nikita Evdokimov on 14.10.22.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func removeExcessCells() {
+        tableFooterView = UIView(frame: .zero)
+    }
+}
